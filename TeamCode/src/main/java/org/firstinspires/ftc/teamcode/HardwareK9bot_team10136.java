fd7d8c2b9ca356e52c8e06a47a14c58f434bd6f3
wnielsen <williamnielsen@gmail.com>
2017-11-07 19:21:22 -0500

    checkin of teamcode from v3.4

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareK9bot_team10136.java
@@ -0,0 +1,124 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a K9 robot.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Servo channel:  Servo to raise/lower arm: "arm"
+ * Servo channel:  Servo to open/close claw: "claw"
+ *
+ * Note: the configuration of the servos is such that:
+ *   As the arm servo approaches 0, the arm position moves up (away from the floor).
+ *   As the claw servo approaches 0, the claw opens up (drops the game element).
+ */
+public class HardwareK9bot_team10136 {
+    /* Public OpMode members. */
+    public DcMotor  leftFrontDrive  = null;
+    public DcMotor  rightFrontDrive = null;
+    public DcMotor  leftBackDrive   = null;
+    public DcMotor  rightBackDrive  = null;
+    public Servo    arm             = null;
+    public Servo    leftClaw        = null;
+    public Servo    rightClaw       = null;
+
+    // Arm
+    public final static double  VERTICAL_ARM_HOME = 1.0;
+    public final static double  HORIZONTAL_ARM_HOME = -0.6;
+    public static double        ARM_MIN_RANGE  = 0.20;
+    public static double        ARM_MAX_RANGE  = 0.90;
+
+    // Claw
+    public final static double LEFT_CLAW_HOME = 0.2;
+    public final static double LEFT_CLAW_MIN_RANGE  = 0.2;
+    public final static double LEFT_CLAW_MAX_RANGE  = 0.7;
+
+    public final static double RIGHT_CLAW_HOME = 0.7;
+    public final static double RIGHT_CLAW_MIN_RANGE  = 0.7;
+    public final static double RIGHT_CLAW_MAX_RANGE  = 0.2;
+
+    /* Local OpMode members. */
+    HardwareMap hwMap  = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareK9bot_team10136() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftFrontDrive  = hwMap.get(DcMotor.class, "left front");
+        rightFrontDrive = hwMap.get(DcMotor.class, "right front");
+        leftBackDrive  = hwMap.get(DcMotor.class, "left back");
+        rightBackDrive = hwMap.get(DcMotor.class, "right back");
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        arm  = hwMap.get(Servo.class, "arm");
+        arm.setPosition(VERTICAL_ARM_HOME);
+
+        leftClaw = hwMap.get(Servo.class, "leftClaw");
+        rightClaw = hwMap.get(Servo.class, "rightClaw");
+
+        leftClaw.setPosition(LEFT_CLAW_HOME);
+        rightClaw.setPosition(RIGHT_CLAW_HOME);
+    }
+}
